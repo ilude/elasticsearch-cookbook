8847f2bb9e425e31365ec0e0cf144c727f9e2b8c
Mike Glenn <mike.glenn@rammount.com>
2015-12-29 17:35:24 -0500

    version 1.7.4

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ maintainer_email 'mglenn@ilude.com'
 license          'All rights reserved'
 description      'Installs/Configures elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.8'
